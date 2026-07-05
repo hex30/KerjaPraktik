e3c81277ad186e2fd3507005950d3ae703984eb1
{username} <fahriramdhani127@gmail.com>
2026-07-05 09:15:24 +0700

    fix: UI bug

--- fix_astro.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/pages/admin/index.astro', 'utf8');
-
-c = c.replace(/<span class="text-\[10px\] font-bold text-slate-400">Baru Saja<\/span>/g, '');
-c = c.replace(/href="\/admin\/route"/g, 'href="/admin/bookings/route"');
-c = c.replace(/href="\/admin\/charter"/g, 'href="/admin/bookings/charter"');
-
-fs.writeFileSync('src/pages/admin/index.astro', c);
-console.log('Fixed index.astro!');
